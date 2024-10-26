ed39be1beeb25ea6f0662e577b372d2a6210c360
ugtfo <90002772+ugtfo@users.noreply.github.com>
2024-10-26 14:54:51 +0300

    Update test_task.py
    error

--- test_task.py
+++ test_task.py
@@ -100,7 +100,8 @@ class TestUserInteraction(unittest.TestCase):
 
         # Проверяем, что возвращаемое значение содержит нужный voucher_id
         self.assertIsNotNone(result)  # Убедитесь, что результат не None
-        self.assertIn({'voucher_id': 1, 'discount': 20}, result)  # Проверяем содержимое результата
+        ##self.assertIn({'voucher_id': 1, 'discount': 20}, result)  # Проверяем содержимое результата
+        self.assertIn({'voucher_id': 1, 'discount': 20}, None)  # Проверяем заведомо неправильное содержимое результата
         
     
 
